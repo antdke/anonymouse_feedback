a0e7461c676d9cf29097a51d5560560d041f1e76
anthony.dke@gmail.com <anthonydike@nyu.edu>
2022-12-20 19:04:23 -0500

    more db validations

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,10 +1,16 @@
 class Feedback < ApplicationRecord
+  before_create :sanitize
   after_create :send_tweet
 
   validates :recipient_handle, :text, presence: true
-  validates :recipient_handle, length: {maximum: 15}
-  validates :text, length: {maximum: 250}
+  validates :recipient_handle, length: {in: 1..15}
+  validates :text, length: {in: 1..250}
 
+  def sanitize
+    self.recipient_handle = self.recipient_handle.gsub("@", "")
+  end
+  
+  
   def send_tweet
     puts "Sending Tweet!"
     # TwitterService.tweet!(self)
